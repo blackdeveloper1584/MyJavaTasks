5f184f7dc0d253ba11f09a2db2212fd724e4dc5b
blackdeveloper1584 <90499029+blackdeveloper1584@users.noreply.github.com>
2022-05-23 22:39:52 +0300

    Delete Course1/Term_2/modul_2/Laboratorna_10 directory

--- Course1/Term_2/modul_2/Laboratorna_10/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.laboratorna_10 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.laboratorna_10 to javafx.fxml;
-    exports com.example.laboratorna_10;
-}
